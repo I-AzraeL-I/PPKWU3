e19a805a7935bbe88014fdd7abebdcff874077bd
I-AzraeL-I <qaraqan70@gmail.com>
2021-11-09 13:40:11 +0100

    Create request and response utility methods for xml and json data

--- src/main/java/com/mycompany/stringapiwrapper/controller/StringFileController.java
+++ src/main/java/com/mycompany/stringapiwrapper/controller/StringFileController.java
@@ -1,7 +1,11 @@
 package com.mycompany.stringapiwrapper.controller;
 
+import com.mycompany.stringapiwrapper.controller.dto.Statistics;
 import com.mycompany.stringapiwrapper.service.StringApiClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +19,38 @@ public class StringFileController {
 
     private final StringApiClient stringApiClient;
 
-    @GetMapping("/is-word")
-    public ResponseEntity<Boolean> isAlpha(@RequestParam("data") String data) {
-        return stringApiClient.isAlpha(data);
+    @GetMapping(value = "/statistics", produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+    })
+    public ResponseEntity<Statistics> getStatistics(@RequestParam String data, @RequestParam String format) {
+        var body = stringApiClient.getStatistics(data).getBody();
+        return prepareRequest(body, format);
+    }
+
+    private <T> ResponseEntity<T> prepareRequest(T body, String format) {
+        var mediaType = parseMediaType(format);
+        if (mediaType == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        var headers = getContentHeaders(mediaType);
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(body);
+    }
+
+    private HttpHeaders getContentHeaders(MediaType mediaType) {
+        var responseHeaders = new HttpHeaders();
+        responseHeaders.setContentType(mediaType);
+        return responseHeaders;
+    }
+
+    private MediaType parseMediaType(String format) {
+        switch (format) {
+            case "json":
+                return MediaType.APPLICATION_JSON;
+            case "xml":
+                return MediaType.APPLICATION_XML;
+            default:
+                return null;
+        }
     }
 }
--- src/main/java/com/mycompany/stringapiwrapper/service/StringFileService.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.stringapiwrapper.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class StringFileService {
-}
