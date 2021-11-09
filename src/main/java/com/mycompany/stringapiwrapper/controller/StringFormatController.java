22b6d371d8bf4cfbde2e54d3fe434c85c91ff1a8
I-AzraeL-I <qaraqan70@gmail.com>
2021-11-09 13:50:20 +0100

    Give controller more meaninful name

--- src/main/java/com/mycompany/stringapiwrapper/controller/StringFormatController.java
+++ src/main/java/com/mycompany/stringapiwrapper/controller/StringFormatController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-public class StringFileController {
+public class StringFormatController {
 
     private final StringApiClient stringApiClient;
 
