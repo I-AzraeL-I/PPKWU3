1552031a7dae614e1231e15cdcec4b76364dc500
I-AzraeL-I <qaraqan70@gmail.com>
2021-11-09 12:22:20 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/stringapiwrapper/StringApiWrapperApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.stringapiwrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringApiWrapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringApiWrapperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycompany/stringapiwrapper/StringApiWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.stringapiwrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringApiWrapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
